9570a8dc51e66cbd34416b4772654450b4a67e16
jiahua <781913268@qq.com>
2017-01-12 16:49:44 +0800

    格式化

--- main/src/main/java/com/xy/study/orm/Generator.java
+++ main/src/main/java/com/xy/study/orm/Generator.java
@@ -32,7 +32,7 @@ public class Generator
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         //指定 逆向工程配置文件
-        File configFile = new File("generatorConfig.xml");
+        File configFile = new File("/home/jiahua/IdeaProjects/Spring/main/src/main/resources/mybatis/generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
